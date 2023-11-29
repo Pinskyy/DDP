67cc772d38f136a3fc0633b0b8b40c1b721243c8
Pinskyy <80452094+Pinskyy@users.noreply.github.com>
2023-11-29 11:31:34 +0700

    Add files via upload

---
+++ Task8.3.py
@@ -0,0 +1,8 @@
+def data_diri(nama, alamat, gender, umur, hobby):
+    print("Nama\t: ", nama)
+    print("Alamat\t: ", alamat)
+    print("Gender\t: ", gender)
+    print("Umur\t: ", umur)
+    print("Hobby\t: ", hobby)
+
+data_diri("Izmil Arifin", "Bogor", "Laki-laki", "18", "Membaca Novel")
