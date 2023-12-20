c7c3273cd3c3e822e7c0f40e6a3a6ccde56d1289
Pinskyy <80452094+Pinskyy@users.noreply.github.com>
2023-12-20 08:01:16 +0700

    Add files via upload

---
+++ Week 11/Gempa.py
@@ -0,0 +1,27 @@
+class Gempa:
+    def __init__(self, lokasi, skala):
+        self.lokasi = lokasi
+        self.skala = skala
+
+    def dampak(self):
+        if self.skala < 2:
+            return "Dampak gempa tidak berasa."
+        elif 2 <= self.skala < 4:
+            return "Dampak gempa: bangunan retak-retak."
+        elif 4 <= self.skala < 6:
+            return "Dampak gempa: bangunan roboh."
+        else:
+            return "Dampak gempa: bangunan roboh, berpotensi tsunami."
+
+if __name__ == "__main__":
+    gempa1 = Gempa("Banten", 1.2)
+    gempa2 = Gempa("Palu", 6.1)
+    gempa3 = Gempa("Cianjur", 5.6)
+    gempa4 = Gempa("Jayapura", 3.3)
+    gempa5 = Gempa("Garut", 4.0)
+
+    print(f"Gempa 1 ({gempa1.lokasi}): {gempa1.dampak()}")
+    print(f"Gempa 2 ({gempa2.lokasi}): {gempa2.dampak()}")
+    print(f"Gempa 3 ({gempa3.lokasi}): {gempa3.dampak()}")
+    print(f"Gempa 4 ({gempa4.lokasi}): {gempa4.dampak()}")
+    print(f"Gempa 5 ({gempa5.lokasi}): {gempa5.dampak()}")
---
+++ Week 11/info_gempa.py
@@ -0,0 +1,13 @@
+from Gempa import Gempa
+
+gempa1 = Gempa("Banten", 1.2)
+gempa2 = Gempa("Palu", 6.1)
+gempa3 = Gempa("Cianjur", 5.6)
+gempa4 = Gempa("Jayapura", 3.3)
+gempa5 = Gempa("Garut", 4.0)
+
+print(f"Gempa 1 ({gempa1.lokasi}): {gempa1.dampak()}")
+print(f"Gempa 2 ({gempa2.lokasi}): {gempa2.dampak()}")
+print(f"Gempa 3 ({gempa3.lokasi}): {gempa3.dampak()}")
+print(f"Gempa 4 ({gempa4.lokasi}): {gempa4.dampak()}")
+print(f"Gempa 5 ({gempa5.lokasi}): {gempa5.dampak()}")
