c12c858af553bd1d05158decb5ba8a13a4ec7091
Pinskyy <80452094+Pinskyy@users.noreply.github.com>
2023-12-21 21:22:39 +0700

    Delete Task 9.3.py

--- Task 9.3.py
+++
@@ -1,7 +0,0 @@
-def duplikat_buah(fruits):
-    fruits_duplikat = [buah for buah in fruits for _ in range(2)]
-    return fruits_duplikat
-
-fruits = ["pepaya", "mangga", "pisang", "durian", "jambu"]
-buah_duplikat = duplikat_buah(fruits)
-print(buah_duplikat)
