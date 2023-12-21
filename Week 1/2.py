6f555043f0854138df76be82aaf436f72d536a77
Pinskyy <80452094+Pinskyy@users.noreply.github.com>
2023-12-21 21:15:02 +0700

    Delete Week 1/2.py

--- Week 1/2.py
+++
@@ -1,6 +0,0 @@
-# Mencetak data yang telah dimasukkan
-print("\nData Anda:")
-print("Nama \t: Abdullah Qa'id Mu'aadz")
-print("NIM \t: 0110223082")
-print("Kelas \t: TI03")
-print("Alamat \t: Ciawi")
